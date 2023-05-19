3079c810a0b1ac0130b609eee661334f77e321c8
gerges2 <gergesvictor512@gmail.com>
2023-05-19 16:55:01 +0300

    add multer pakage

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const PORT=process.env.PORT 
 const url =process.env.URL
 const express= require ("express");
-// const User = require("./model/user");
-
-const userRouter=require("./routes/user.js");
-
+const path = require('path');
+// const multer  = require('multer')
+//   const userRouter=require("./routes/user.js");
 const speakers=require("./routes/SpeakerRouter.js");
 const Authentication=require("./routes/AuthenticationRouter.js");
 const Student=require("./routes/StudentRouter.js");
@@ -14,22 +13,31 @@ const mongoose= require("mongoose");
 const app= express();
 app.use(express.json())
 
-
-
+app.use(express.urlencoded({ extended:false}))
 async function connect(){
     let dat=   await mongoose.connect(url,{useNewUrlParser: true, useUnifiedTopology: true})
 console.log(typeof(dat) )}
 connect();
-
-// app.use(["/user",'/users'],userRouter)
-
-
-
 app.use(["/speakers"],speakers)
 app.use(["/events"],Event)
 app.use(["/students"],Student)
 app.use(["/login","registerStudent"],Authentication)
 
+
+
+
+const multer  = require('multer')
+const upload = multer({ dest: 'images/' })
+
+app.post('/upload', upload.single('url'), function (req, res) {
+   console.log(req.file)
+   console.log("djd")
+   console.log( req.body)
+   res.send("done")
+});
+
+
+  
 
 
 app.listen(PORT,(err)=>{
